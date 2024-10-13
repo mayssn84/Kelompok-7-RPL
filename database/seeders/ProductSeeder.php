b8a43a02bd211d2f6a22f20dbe7d908b12c22ada
Maisun Mufliha Aprilia.M <146790479+mayssn84@users.noreply.github.com>
2024-10-13 23:26:59 +0800

    Update ProductSeeder.php

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -13,10 +13,12 @@ class ProductSeeder extends Seeder
     public function run(): void
     {
         $products = [
-            ['name' => 'Kacang mente', 'description' => 'Kacang khas Kendari', 'price' => 30000],
-            ['name' => 'Abon Ikan Marni', 'description' => 'Abon Ikan', 'price' => 50000],
-            ['name' => 'Kain Tenun Khas Kendari', 'description' => 'Kain tenun khas', 'price' => 150000],
-            ['name' => 'Gula Ni’i', 'description' => 'Gula aren', 'price' => 20000],
+            ['name' => 'Kacang mete', 'description' => 'Kacang khas Kendari', 'price' => 40000],
+            ['name' => 'Abon Ikan Marlin', 'description' => 'Abon Ikan Marlin Khas', 'price' => 30000],
+            ['name' => 'Kain Tenun Khas Kendari', 'description' => 'Kain tenun khas', 'price' => 70000],
+            ['name' => 'Bagea', 'description' => 'Kue Khas kendari', 'price' => 35000],
+            ['name' => 'Brownies Mete', 'description' => 'Brownies Khas Kendari', 'price' => 60000],
+            ['name' => 'Souvenir Khas Kendari', 'description' => 'Souvenir  Khas', 'price' => 20000],
         ];
     }
 }
